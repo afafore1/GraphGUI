b38fef31b805aad4743b18466ceac9b67373a7a9
Ayomitunde Fafore <Ayomitunde Fafore>
2015-11-15 13:00:14 -0500

    added algorithm to run bfs

--- Graphify/src/graphify/Graphify.java
+++ Graphify/src/graphify/Graphify.java
@@ -6,20 +6,32 @@ import java.awt.Image;
 import java.awt.Point;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Graphify extends javax.swing.JFrame {
+
     HashMap<Integer, ArrayList<Integer>> nodes = new HashMap();
+    private Queue<Integer> queue;
     HashMap<Integer, Point> locations = new HashMap();
-    int selectedNode = -1;
-    int SIZE_OF_NODE = 20;
+    private int[] distTo;
+    private LinkedHashMap<Integer, Integer> set = new LinkedHashMap<Integer, Integer>();
+    int[] visited;
+    int _selectedNode = -1;
+    int _SIZE_OF_NODE = 20;
     int id = 0;
+    static int _source = 0;
     Image bufferImage;
     Graphics bufferGraphic;
+
     public Graphify() {
         initComponents();
         bufferImage = createImage(pnlGraph.getWidth(), pnlGraph.getHeight());
         bufferGraphic = bufferImage.getGraphics();
+        queue = new LinkedList<Integer>();
     }
+
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
@@ -38,6 +50,9 @@ public class Graphify extends javax.swing.JFrame {
             }
         });
         pnlGraph.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                pnlGraphMouseClicked(evt);
+            }
             public void mousePressed(java.awt.event.MouseEvent evt) {
                 pnlGraphMousePressed(evt);
             }
@@ -106,8 +121,8 @@ public class Graphify extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void pnlGraphMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_pnlGraphMousePressed
-        selectedNode = nodeSelected(evt.getX(), evt.getY());
-        if (selectedNode < 0) {
+        _selectedNode = nodeSelected(evt.getX(), evt.getY());
+        if (_selectedNode < 0) {
             nodes.put(id, new ArrayList());
             locations.put(id++, new Point(evt.getX(), evt.getY()));
         }
@@ -115,11 +130,11 @@ public class Graphify extends javax.swing.JFrame {
     }//GEN-LAST:event_pnlGraphMousePressed
 
     private void pnlGraphMouseDragged(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_pnlGraphMouseDragged
-        if (selectedNode >= 0) {
+        if (_selectedNode >= 0) {
             Image buff = createImage(pnlGraph.getWidth(), pnlGraph.getHeight());
             Graphics buffG = buff.getGraphics();
             buffG.drawImage(bufferImage, 0, 0, this);
-            Point source = locations.get(selectedNode);
+            Point source = locations.get(_selectedNode);
             buffG.drawLine(source.x, source.y,
                     evt.getX(), evt.getY());
             pnlGraph.getGraphics().drawImage(buff, 0, 0, this);
@@ -132,27 +147,89 @@ public class Graphify extends javax.swing.JFrame {
     }//GEN-LAST:event_pnlGraphComponentResized
 
     private void pnlGraphMouseReleased(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_pnlGraphMouseReleased
-        if (selectedNode >= 0) {
+        if (_selectedNode >= 0) {
             int destination = nodeSelected(evt.getX(), evt.getY());
-            if (destination >= 0 && destination != selectedNode) {
-                nodes.get(selectedNode).add(destination);
-                nodes.get(destination).add(selectedNode);
-                selectedNode = -1;
+            if (destination >= 0 && destination != _selectedNode) {
+                nodes.get(_selectedNode).add(destination);
+                nodes.get(destination).add(_selectedNode);
+                _selectedNode = -1;
             }
         }
         graph();
     }//GEN-LAST:event_pnlGraphMouseReleased
 
-    private void btnReset1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnReset1ActionPerformed
-        for (int i = 0; i < nodes.size(); i++) {
-            ArrayList<Integer> connections = (ArrayList<Integer>) nodes.values().toArray()[i];
-            int key = (Integer) nodes.keySet().toArray()[i];
-            System.out.println("Node number " + key + ":");
-            for (Integer theConnection: connections) {
-                System.out.println("\tConnects with: " + theConnection);
+    public ArrayList<Integer> getEdge(int source) {
+        if (source > nodes.size()) {
+            System.out.println("Vertex not present");
+            return null;
+        }
+        return nodes.get(source);
+    }
+
+    void bfs(int source) {
+        int V = nodes.size();
+        distTo = new int[V];
+        visited = new int[V];
+        for (int i = 0; i < V; i++) {
+            visited[i] = -1;
+        }
+        ArrayList<Integer> conn = new ArrayList<Integer>();
+        int i, element;
+        visited[source] = 0;
+        queue.add(source);
+        while (!queue.isEmpty()) {
+            element = queue.remove();
+            System.out.println(element + " removed");
+            i = element;
+            conn.add(element);
+            ArrayList<Integer> iList = getEdge(i);
+            int x = 0;
+            while (x < iList.size()) {
+                if (visited[iList.get(x)] == -1) {
+                    queue.add(iList.get(x));
+                    visited[iList.get(x)] = i;
+                    distTo[iList.get(x)] = distTo[i] + 1;
+                }
+                x++;
             }
         }
+        System.out.println("Order is " + conn);
+    }
+
+    public int hasPath(int v) {
+        return visited[v];
+    }
+
+    public int distTo(int v) {
+        return distTo[v];
+    }
+
+    public void shortestPath(int v, int e) {
+        if (e == v) {
+            System.out.println(v + "-->" + v);
+            System.exit(0);
+        }
+        for (int i = e; i > 0; i = visited[i]) {
+            if (i == v) {
+                break;
+            }
+            if (visited[i] != -1) {
+                set.put(i, visited[i]);
+            }
+        }
+        String dset = set.toString().replace("}", "").replace("{", "");
+        dset = new StringBuilder(dset).reverse().toString().replaceAll("=", "-->");
+        System.out.println(dset);
+    }
+    //[0, 2, 19, 5, 7, 9, 14]
+
+    private void btnReset1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnReset1ActionPerformed
+        for (int i = 0; i < nodes.size(); i++) {
+            System.out.println(i + "->" + getEdge(i));
+        }
+        System.out.println("Source is: " + _source);
     }//GEN-LAST:event_btnReset1ActionPerformed
+
 
     private void btnResetActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnResetActionPerformed
         nodes = new HashMap();
@@ -161,13 +238,24 @@ public class Graphify extends javax.swing.JFrame {
         graph();
     }//GEN-LAST:event_btnResetActionPerformed
 
+    private void pnlGraphMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_pnlGraphMouseClicked
+        _selectedNode = nodeSelected(evt.getX(), evt.getY());
+        if (evt.getClickCount() == 2) {
+            set.clear();
+            _source = _selectedNode;
+            bfs(_source);
+            shortestPath(_source, nodes.size() - 1);
+        }
+
+    }//GEN-LAST:event_pnlGraphMouseClicked
+
     private void graph() {
         bufferGraphic.setColor(Color.white);
         bufferGraphic.fillRect(0, 0, pnlGraph.getWidth(), pnlGraph.getHeight());
         bufferGraphic.setColor(Color.black);
         for (int i = 0; i < locations.size(); i++) {
             Point thePoint = (Point) locations.values().toArray()[i];
-            for (Integer destinationIndex: (ArrayList<Integer>) nodes.values().toArray()[i]) {
+            for (Integer destinationIndex : (ArrayList<Integer>) nodes.values().toArray()[i]) {
                 Point destinantionPoint = locations.get(destinationIndex);
                 bufferGraphic.drawLine(thePoint.x, thePoint.y,
                         destinantionPoint.x, destinantionPoint.y);
@@ -175,42 +263,43 @@ public class Graphify extends javax.swing.JFrame {
         }
         for (int i = 0; i < locations.size(); i++) {
             Point thePoint = (Point) locations.values().toArray()[i];
-            if (locations.keySet().toArray()[i] == (Integer) selectedNode) {
+            if (locations.keySet().toArray()[i] == (Integer) _selectedNode) {
                 bufferGraphic.setColor(Color.orange);
             } else {
                 bufferGraphic.setColor(Color.red);
             }
-            bufferGraphic.fillOval(thePoint.x - SIZE_OF_NODE / 2, 
-                    thePoint.y - SIZE_OF_NODE / 2, SIZE_OF_NODE, SIZE_OF_NODE);
+            bufferGraphic.fillOval(thePoint.x - _SIZE_OF_NODE / 2,
+                    thePoint.y - _SIZE_OF_NODE / 2, _SIZE_OF_NODE, _SIZE_OF_NODE);
         }
 
         bufferGraphic.setColor(Color.blue);
         for (int i = 0; i < locations.size(); i++) {
             Point thePoint = (Point) locations.values().toArray()[i];
             bufferGraphic.drawString("V " + locations.keySet().toArray()[i],
-                    thePoint.x + SIZE_OF_NODE + 4, thePoint.y + SIZE_OF_NODE + 4);
+                    thePoint.x + _SIZE_OF_NODE + 4, thePoint.y + _SIZE_OF_NODE + 4);
         }
         pnlGraph.getGraphics().drawImage(bufferImage, 0, 0, this);
     }
-    
-    private int nodeSelected (int x, int y) {
+
+    private int nodeSelected(int x, int y) {
         for (int i = 0; i < locations.size(); i++) {
             Point thePoint = (Point) locations.values().toArray()[i];
-            int deltaX = x - (thePoint.x - SIZE_OF_NODE / 2);
-            int deltaY = y - (thePoint.y - SIZE_OF_NODE / 2);
+            int deltaX = x - (thePoint.x - _SIZE_OF_NODE / 2);
+            int deltaY = y - (thePoint.y - _SIZE_OF_NODE / 2);
             if (Math.sqrt(deltaX * deltaX
-                    + deltaY * deltaY) <= SIZE_OF_NODE) {
+                    + deltaY * deltaY) <= _SIZE_OF_NODE) {
                 return (int) locations.keySet().toArray()[i];
             }
         }
         return -1;
     }
-    
+
     @Override
     public void paint(Graphics g) {
         super.paint(g);
         graph();
     }
+
     /**
      * @param args the command line arguments
      */
